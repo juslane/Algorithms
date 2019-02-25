57df5fdd733eea81f8ad111097b64ea874634c27
juslane <44763263+juslane@users.noreply.github.com>
2019-02-25 13:13:29 -0800

    Add files via upload
    A guessing game where you give the computer a range of numbers. A random number is selected in that range, and the computer will begin to guess until it gets the number right.  Will always succeed in the stated number of tries!

---
+++ Binary Search.py
@@ -0,0 +1,54 @@
+# Binary Search Algorithm
+
+import random
+import numpy as np
+import math
+
+COUNT = 0
+MIN = 1
+MAX = 1
+
+def numbers():
+    global MAX
+    set_n = input('Numbers will be from 1 to: ')
+    MAX = int(set_n)
+    max_guesses = np.log2(MAX)
+    print('Computer will guess correctly in no more than %d tries!' \
+          % math.ceil(max_guesses))
+
+def select_number(disp = True):
+    global MIN, MAX, number
+    number = random.randint(MIN, MAX)
+    if disp == True:
+        print('\nComputer Selected: %s\n' % number)
+
+def guess_avg(disp = True):
+    global MIN, MAX, avg
+    avg = math.floor((MIN + MAX) / 2)
+    if disp == True:
+        print('Guessing %s\n' % avg)
+
+def high_low():
+    global number, avg, MAX, MIN
+    if avg > number:
+        print('Guess was too high.\n')
+        MAX = avg - 1
+        main()
+    elif avg < number:
+        print('Guess was too low.\n')
+        MIN = avg + 1
+        main()
+    else:
+        print('You got it! %d is correct!' % number)
+
+def main():
+    global COUNT
+    COUNT += 1
+    print('Guess # %d\n' % COUNT)
+    guess_avg()
+    high_low()
+
+if __name__ == '__main__':
+    numbers()
+    select_number()
+    main()
